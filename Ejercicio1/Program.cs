12d81c8c5e8b91d9a881ed75a57a316bb56815b0
IsabellaMuroni <isa_sk_@hotmail.com>
2019-05-12 21:17:44 -0300

    subiendo

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, numInv;
+            Console.Write("Ingrese un número");
+            num = int.Parse(Console.ReadLine());
+            numInv = Invertir(num);
+            Console.Write("El número invertido es: ");
+            Console.Write(numInv);
+            Console.ReadKey();
+        }
+        static public int Invertir(int numero)
+        {
+            int invertido = 0;
+
+            while (numero > 0)
+            {
+                invertido = (invertido * 10) + (numero % 10);
+                numero = numero / 10;
+            }
+
+            return invertido;
+        }
+    }
+}
