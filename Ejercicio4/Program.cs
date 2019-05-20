52c5dae3a2a3fcb7ffd9293f0eede2d7787d8b51
IsabellaMuroni <isa_sk_@hotmail.com>
2019-05-19 23:12:15 -0300

    Ejercicio 4

--- Ejercicio4/Program.cs
+++ Ejercicio4/Program.cs
@@ -10,6 +10,24 @@ namespace Ejercicio4
     {
         static void Main(string[] args)
         {
+            string cadena, cad1;
+
+            Console.WriteLine("Ingrese una cadena de texto: ");
+            cadena = Console.ReadLine();
+
+            string Mayusc = cadena.ToUpper();
+            string minus = cadena.ToLower();
+            int longitud = cadena.Length;
+            Console.WriteLine("La longitud de la cadena es: {0}", longitud);
+            Console.WriteLine(Mayusc);
+            Console.WriteLine(minus);
+            Console.WriteLine("Ingrese otra cadena de texto: ");
+            cad1 = Console.ReadLine();
+            var S = String.Concat(cadena, cad1);
+            Console.Write(S);
+            string.Compare(cadena, cad1);
+
+            Console.ReadKey();
         }
     }
 }
